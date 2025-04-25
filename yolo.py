5efd8b4a9eb417400676479559ae558e90a7a856
sevenamplee <johnny20030114@gmail.com>
2025-04-25 16:58:28 +0800

    v2.5

--- yolo.py
+++ yolo.py
@@ -22,12 +22,14 @@ class YOLOv9Node(Node):
         self.stop_publisher = self.create_publisher(Int64, '/stop_signal', 10)  # 停止信號發布
         self.class_count = {}  # 記錄偵測次數
         self.class_last_detected = {}  # 記錄上次偵測的時間
-        self.threshold = 5  # 門檻值
+        self.threshold = 5 # 門檻值
         self.wait_time = 10  # 設定等待時間 (毫秒)  # 設定等待時間 (秒)
         self.get_logger().info("YOLOv9 Node Initialized and subscribed to /image/image_raw")
         self.width = None
         self.height = None
-    
+        self.num = 1
+
+
     def image_callback(self, msg):
         # 轉換 ROS2 影像訊息到 OpenCV 格式
         image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
@@ -46,13 +48,18 @@ class YOLOv9Node(Node):
             self.width = x_max - x_min
             self.height = y_max - y_min
             if (class_id ==7 and self.width>self.height):
-                    stop_msg = Int64()
-                    stop_msg.data = 0
-                    self.stop_publisher.publish(stop_msg)
-            else:
+                stop_msg = Int64()
+                stop_msg.data = 0
+                self.stop_publisher.publish(stop_msg)
+                self.num =1
+                print("STOP")
+                
+            elif(self.num ==1):
                 stop_msg = Int64()
                 stop_msg.data = 1
                 self.stop_publisher.publish(stop_msg)
+                print("go")
+                self.num = 0
             cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
             label = f"Class {class_id}: {confidence:.2f}"
             cv2.putText(image, label, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
@@ -81,8 +88,9 @@ class YOLOv9Node(Node):
                 msg.data = class_id
                 self.publisher.publish(msg)
                 self.get_logger().info(f"Published class ID: {class_id}")
-                if (class_id==3):
+                if (class_id==2 or 3 or 5 or 6 ):
                     time.sleep(10)
+                self.class_count[class_id] =0
 
         
         # 顯示結果影像
