45dbd44d6e79e07abd164da7376a7853ac1c8d85
sevenamplee <johnny20030114@gmail.com>
2025-04-25 16:59:35 +0800

    v4.5 移除停車 新增綠燈停止

--- twoline_follower.py
+++ twoline_follower.py
@@ -30,6 +30,7 @@ W_sampling_4 = 200
 
 num1 = 0
 num2 = 0
+num3 = 0
 direction=0
 class Lane_detection(Node):
 
@@ -54,6 +55,7 @@ class Lane_detection(Node):
             depth=10,
             reliability=QoSReliabilityPolicy.SYSTEM_DEFAULT  # 設定為系統預設值
         )
+        self.stop_publisher = self.create_publisher(Int64, '/stop_signal', 1)  # 停止信號發布
         self.liadr_subscription = self.create_subscription(LaserScan, '/scan', self.scan_callback,qos_profile)
         self.liadr_subscription 
         self.num = 5
@@ -63,6 +65,12 @@ class Lane_detection(Node):
         self.lidar270 = None
         self.lidar0 = None
         self.previous_mode = None
+        self.lidar165 = None
+        self.lidar_neg165 = None
+        self.entered_class3 = None  
+        self.start_time = time.time()  # 啟動計時
+
+
 
     # 類別外的全域變數方式（或你可以放成 self.previous_mode）
     
@@ -86,9 +94,9 @@ class Lane_detection(Node):
         index_minus0 = int(math.radians(180) /angle_increment) # 弧度（角度）/ 角度增量
         index_minus1 = int(math.radians(90) /angle_increment) # 弧度（角度）/ 角度增量
         index_minus2 = int(math.radians(-90) /angle_increment)
-        # index_minus3 = int(math.radians(0) /angle_increment)
+        index_minus3 = int(math.radians(0) /angle_increment)
 
-        # self.lidar0 = msg.ranges[index_minus3]
+        self.lidar0 = msg.ranges[index_minus3]
         self.lidar90 = msg.ranges[index_minus1]
         self.lidar180 = msg.ranges[index_minus0]
         self.lidar270 = msg.ranges[index_minus2]
@@ -96,20 +104,40 @@ class Lane_detection(Node):
         左右循線
     '''
     def two_line(self, msg, kernel_size=25, low_threshold=10, high_threshold=20, close_size=5):
-        global num1 ,direction
+        global num1 ,direction,num2,num3
         # 將ROS Image轉換成OpenCV格式
         img = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-        R_loss = L_loss = False
+        R_loss = L_loss =False
         # 左右線極限X值(需重置)
         L_min_300 = L_min_240 = L_min_180 = L_min_140 = 0
-        R_min_300 = R_min_240 = R_min_180 = R_min_140 = 640
+        R_min_300 = R_min_240=R_min_180 =R_min_140 = 640
+        
+        G_H_low, G_S_low, G_V_low = 45,150,150
+        G_H_high, G_S_high, G_V_high = 85,255,255
         # 影像預處理
         # img = copy(img)
         img = cv2.resize(img,(640,360))
+
         # img = cv2.GaussianBlur(img, (11, 11), 0)
         hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-
+        # 只保留影像上半部
         # 右線遮罩
+                # 綠色遮罩區域（在霍夫運算之前加入）
+
+        lower_G = np.array([G_H_low, G_S_low, G_V_low])
+        upper_G = np.array([G_H_high, G_S_high, G_V_high])
+        mask_G = cv2.inRange(hsv, lower_G, upper_G)
+        green_pixel_count = cv2.countNonZero(mask_G)
+        cv2.imshow("mask_L", mask_G)
+
+        if green_pixel_count < 10 and num2==0:  # 若綠色像素過少（可視情況調整閾值）
+
+            self.switch_mode("STOP - Detected")
+            return
+            
+        else :
+            num2 +=1
+
         lower_R = np.array([R_H_low,R_S_low,R_V_low])
         upper_R = np.array([R_H_high,R_S_high,R_V_high])
         mask_R = cv2.inRange(hsv,lower_R,upper_R)
@@ -194,46 +222,81 @@ class Lane_detection(Node):
         R_min = ((R_min_300+R_min_240+R_min_180+R_min_140)/4)-320
         L_min = ((L_min_300+L_min_240+L_min_180+L_min_140)/4)
         R_target_line = int(R_min-265)
-        L_target_line = int(L_min-55)
+        L_target_line = int(L_min-70)
         target_line = None
-# 停車模式
-        if (self.class_id == 3):
-            if(self.lidar90 < 0.1 or self.lidar270 < 0.1 ):
-                if(self.lidar90 < 0.1):
-                    direction = 1
-                elif (self.lidar270 < 0.1):
-                    direction = -1
-            elif not(direction):
-                target_line = L_target_line
-                self.switch_mode("parking mode")
-            elif (self.lidar0 >0.15 and direction and num2 == 0 ):
-                target_line = -200*direction
-                self.switch_mode("turn")
-                if (self.lidar0 <0.16):
-                    num2 = 1
-                    self.switch_mode("turn end")
-            elif (self.lidar0 < 0.30 and num2 == 1):
-                target_line = 1 
-                if(self.lidar0 >0.3):
-                    num2 == 2
-            elif (num2 ==2 ):
-                if (self.lidar180 >0.30):
-                    target_line = -200*direction
-                else :
-                    num2 = 3
-            elif(num2 == 3):
-                target_line =1
-                if (self.lidar180 <0.10):
-                    num2 = 4
-            elif(num2 == 4):
-                target_line = 200*direction
-                if(self.lidar90 <0.15 and direction == -1):
-                    direction = 0
-                elif(self.lidar270 <0.15 and direction == 1):
-                    direction = 0
 
-# 避障模式
-        elif (self.class_id == 5):
+        # 計時器
+        if self.class_id == 3 and not self.entered_class3:
+            self.start_time = time.time()
+            self.entered_class3 = True
+            self.switch_mode("Start class 3 timer")
+
+        # 停車模式
+        # if self.class_id == 3:
+        #     elapsed_time = time.time() - self.start_time if self.start_time else 0            # 前10秒，正常循黃線進彎
+            
+        #     if elapsed_time < 10:
+        #         if (L_target_line <= -55 ):
+        #             L_target_line -= self.num    
+        #             self.num +=10
+        #             target_line = int(L_target_line)
+        #             if (L_target_line <=90):
+        #                 L_target_line = -120
+        #         target_line = L_target_line
+        #         self.switch_mode("yellow line follow")
+            
+        #     else:
+        #         if self.lidar90 < 0.3 and num3 == 0:
+        #             direction = 1  # 左邊有障礙，向右偏
+        #             num3 =1
+        #         elif self.lidar270 < 0.3 and num3 == 0:
+        #             direction = -1  # 右邊有障礙，向左偏
+        #             num3 =1
+        #         elif(self.lidar0 > 0.25 and num3 ==1):
+        #             target_line = -200 * direction
+        #             self.switch_mode("turn")
+        #             if(self.lidar0 < 0.30):
+        #                 num3 = 2
+        #                 self.switch_mode("turn done")
+        #         elif num3 ==2 and self.lidar0 <0.30:
+        #             target_line = -4
+        #             self.switch_mode("path")
+        #             if (self.lidar0 > 0.25):
+        #                 self.switch_mode("clear")
+        #                 num3 = 3
+        #         elif(self.lidar180 > 0.45 and num3 ==3):
+        #             target_line = -200 * direction
+        #             self.switch_mode("turn2")
+        #             if(self.lidar180 < 0.45):
+        #                 num3 = 4
+        #                 self.switch_mode("gos2")
+        #         elif (num3 ==4 ):
+        #             self.switch_mode("stop")
+        #             stop_msg = Int64()
+        #             stop_msg.data = 0
+        #             self.stop_publisher.publish(stop_msg)
+        #             num3 =5
+
+        #         elif (num3 ==5):
+        #             time.sleep(5)
+
+        #             num3 = 6
+                
+        #         elif num3 ==6 and self.lidar0 <0.15:
+        #             target_line = -4
+        #             self.switch_mode("path")
+
+        #             if (self.lidar0 > 0.15):
+        #                 self.switch_mode("clear")
+        #                 num3 = 3
+
+        #         else :    
+        #             target_line = -4
+        #             self.switch_mode("path clear")
+
+
+        # 雷達模式
+        if (self.class_id == 5):
             
             if (self.lidar180 <  0.35 or num1 ==1):
                 target_line =  -200
@@ -248,27 +311,35 @@ class Lane_detection(Node):
                     num1 =0                
             elif(num1 == 0) :
                 target_line = int(R_target_line) +20
+                if (R_target_line >= 55 ):
+                    R_target_line += self.num    
+                    self.num +=10 
+                    if (R_target_line >=100):
+                        R_target_line = 100
+                    target_line = int(R_target_line)
                 self.switch_mode("ladir mode")
         
             
             
-# 右循線模式
+        # 右線模式
         elif(L_loss or self.class_id == 6):
             if (R_target_line >= 55 ):
                 R_target_line += self.num    
-                self.num +=5 
-                if (R_target_line >=80):
-                    R_target_line = 80
+                self.num +=10 
+                if (R_target_line >=100):
+                    R_target_line = 100
                 target_line = int(R_target_line)
             else :
                 target_line = int( R_target_line)
-                self.num = 5
+                self.num = 10
             self.switch_mode("R mode")
-# 左循線模式
+            
+
+        # 左線模式
         elif(R_loss or self.class_id == 2 ):
             if (L_target_line <= -55 ):
                 L_target_line -= self.num    
-                self.num +=5
+                self.num +=10
                 target_line = int(L_target_line)
                 if (L_target_line <=90):
                     L_target_line = -100
@@ -276,20 +347,25 @@ class Lane_detection(Node):
                 target_line = int(L_target_line)
                 self.num = 5
             self.switch_mode("L mode ")
-# 正常循線模式
+
+
+        # 雙線模式
         elif not (L_loss and R_loss and self.class_id == 0):
             target_line = int((R_target_line+L_target_line)/2)
             self.switch_mode("normal mode ")
+
+        # 啥都不是
         else:
             self.switch_mode("error")
-# 發布訊息
         if (target_line):
             pub_msg=Int64()
             pub_msg.data=-target_line
             self.publisher_.publish(pub_msg)
+
+
          # 輸出原圖&成果
         cv2.imshow("img", img)
-        # cv2.imshow("mask_L", mask_R)
+        cv2.imshow("mask_L", mask_G)
         cv2.waitKey(1)
 
         # return -target_line, img
